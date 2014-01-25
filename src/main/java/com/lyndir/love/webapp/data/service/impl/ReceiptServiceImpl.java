eb4b45dd09ee41581ca6997e0edf2626a3deb0c2
Maarten Billemont <lhunath@lyndir.com>
2014-01-25 12:40:58 -0500

    Added docs to ReceiptService states.

--- src/main/java/com/lyndir/love/webapp/data/service/impl/ReceiptServiceImpl.java
+++ src/main/java/com/lyndir/love/webapp/data/service/impl/ReceiptServiceImpl.java
@@ -236,15 +236,44 @@ public class ReceiptServiceImpl implements ReceiptService {
         }
 
         public enum Status {
+            /**
+             * The receipt is valid.
+             */
             SUCCESS( 0 ),
+            /**
+             * The App Store could not read the JSON object you provided.
+             */
             INVALID_REQUEST( 21000 ),
+            /**
+             * The data in the receipt-data property was malformed.
+             */
             INVALID_RECEIPT( 21002 ),
+            /**
+             * The receipt could not be authenticated.
+             */
             UNAUTHORIZED_RECEIPT( 21003 ),
+            /**
+             * The shared secret you provided does not match the shared secret on file for your account.
+             */
             UNAUTHORIZED_REQUEST( 21004 ),
+            /**
+             * The receipt server is not currently available.
+             */
             UNAVAILABLE( 21005 ),
+            /**
+             * This receipt is valid but the subscription has expired. When this status code is returned to your server, the receipt data
+             * is also decoded and returned as part of the response.
+             */
             SUBSCRIPTION_EXPIRED( 21006 ),
+            /**
+             * This receipt is a sandbox receipt, but it was sent to the production service for verification.
+             */
             RECEIPT_SANDBOX( 21007 ),
+            /**
+             * This receipt is a production receipt, but it was sent to the sandbox service for verification.
+             */
             RECEIPT_PRODUCTION( 21008 );
+
             private final int status;
 
             Status(int status) {
